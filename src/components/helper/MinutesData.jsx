3b6929c7dae0a687982dec1b61ddba068bc0d896
Dalam-Cajee <dalamcajee1999@gmail.com>
2024-03-19 21:57:00 +0530

    Pagination Minutes NGT

---
+++ src/components/helper/MinutesData.jsx
@@ -0,0 +1,213 @@
+import minutes1 from '../../assets/NGT/Minutes_of_Proceedings_NGT_1st_Sitting_of_Oversight_Committee.pdf'
+import minutes2 from '../../assets/NGT/Minutes_of_Proceedings_NGTCommittee_30thMeeting.pdf'
+import minutes3 from '../../assets/NGT/Minutes_of_Proceedings_NGTCommittee_11th_Special_Sitting.pdf'
+import minutes4 from '../../assets/NGT/Minutes_of_Proceedings_NGTCommittee_29thMeeting.pdf'
+import minutes5 from '../../assets/NGT/Minutes_of_Proceedings_NGTCommittee_10th_Special_Sitting.pdf'
+import minutes6 from '../../assets/NGT/Minutes_of_Proceedings_NGTCommittee_28thMeeting.pdf'
+import minutes7 from '../../assets/NGT/Minutes_of_Proceedings_NGTCommittee_9th_Special_Sitting.pdf'
+import minutes8 from '../../assets/NGT/Minutes_of_Proceedings_NGTCommittee_27thMeeting.pdf'
+import minutes9 from '../../assets/NGT/Minutes_of_Proceedings_NGTCommittee_26thMeeting.pdf'
+import minutes10 from '../../assets/NGT/Minutes_of_Proceedings_NGTCommittee_25thMeeting.pdf'
+import minutes11 from '../../assets/NGT/Minutes_of_Proceedings_NGTCommittee_24thMeeting.pdf'
+import minutes12 from '../../assets/NGT/Minutes_of_Proceedings_NGTCommittee_23rdMeeting.pdf'
+import minutes13 from '../../assets/NGT/Minutes_of_Proceedings_NGTCommittee_22ndMeeting.pdf'
+import minutes14 from '../../assets/NGT/Minutes_of_Proceedings_NGTCommittee_7th_Special_Sitting.pdf'
+import minutes15 from '../../assets/NGT/Minutes_of_Proceedings_NGTCommittee_6th_Special_Sitting.pdf'
+import minutes16 from '../../assets/NGT/Minutes_of_Proceedings_NGTCommittee_21stMeeting.pdf'
+import minutes17 from '../../assets/NGT/Minutes_of_Proceedings_NGTCommittee_5th_Special_Sitting.pdf'
+import minutes18 from '../../assets/NGT/Minutes_of_Proceedings_NGTCommittee_20thMeeting.pdf'
+import minutes19 from '../../assets/NGT/Minutes_of_Proceedings_NGTCommittee_3rd_Special_Sitting.pdf'
+import minutes20 from '../../assets/NGT/Minutes_of_Proceedings_NGTCommittee_19thMeeting.pdf'
+import minutes21 from '../../assets/NGT/Minutes_of_Proceedings_NGTCommittee_18thMeeting.pdf'
+import minutes22 from '../../assets/NGT/Minutes_of_Proceedings_NGTCommittee_2nd_Special_Sitting.pdf'
+import minutes23 from '../../assets/NGT/Minutes_of_Proceedings_NGTCommittee_17thMeeting.pdf'
+import minutes24 from '../../assets/NGT/Minutes_of_Proceedings_NGTCommittee_16thMeeting_2ndday.pdf'
+import minutes25 from '../../assets/NGT/Minutes_of_Proceedings_NGTCommittee_16thMeeting_1stday.pdf'
+import minutes26 from '../../assets/NGT/Minutes_of_Proceedings_NGTCommittee_1st_Special_Sitting.pdf'
+import minutes27 from '../../assets/NGT/Minutes_of_Proceedings_NGTCommittee_15thMeeting.pdf'
+import minutes28 from '../../assets/NGT/Minutes_of_Proceedings_NGTCommittee_Site_Visit_during_14thMeeting.pdf'
+import minutes29 from '../../assets/NGT/Minutes_of_Proceedings_NGTCommittee_14thMeeting.pdf'
+import minutes30 from '../../assets/NGT/Minutes_of_Proceedings_NGTCommittee_13thMeeting.pdf'
+import minutes31 from '../../assets/NGT/Minutes_of_Proceedings_NGTCommittee_12thMeeting.pdf'
+import minutes32 from '../../assets/NGT/Minutes_of_Proceedings_NGTCommittee_11thMeeting.pdf'
+import minutes33 from '../../assets/NGT/Minutes_of_Proceedings_NGTCommittee_10thMeeting.pdf'
+import minutes34 from '../../assets/NGT/Minutes_of_Proceedings_NGTCommittee_9thMeeting.pdf'
+import minutes35 from '../../assets/NGT/Minutes_of_Proceedings_NGTCommittee_8thMeeting.pdf'
+import minutes36 from '../../assets/NGT/Minutes_of_Proceedings_NGTCommittee_7thMeeting.pdf'
+import minutes37 from '../../assets/NGT/Minutes_of_Proceedings_NGTCommittee_6thMeeting.pdf'
+import minutes38 from '../../assets/NGT/Minutes_of_Proceedings_NGTCommittee_5thMeeting.pdf'
+import minutes39 from '../../assets/NGT/Minutes_of_Proceedings_NGTCommittee_4thMeeting.pdf'
+import minutes40 from '../../assets/NGT/Minutes_of_Proceedings_NGTCommittee_3rdMeeting.pdf'
+import minutes41 from '../../assets/NGT/Minutes_of_Proceedings_NGTCommittee_2ndMeeting.pdf'
+import minutes42 from '../../assets/NGT/Minutes_of_Proceedings_NGTCommittee_1stMeeting.pdf'
+
+export const minutesData = [
+  {
+    name: 'Minutes of Proceedings of NGT 1st Sitting of Oversight Committee',
+    url: minutes1,
+  },
+  {
+    name: 'Minutes of Proceedings of NGT Committee - 30th Meeting',
+    url: minutes2,
+  },
+  {
+    name: '11th Special Sitting - Minutes of Proceedings',
+    url: minutes3,
+  },
+  {
+    name: 'Minutes of Proceedings of NGT Committee - 29th Meeting',
+    url: minutes4,
+  },
+  {
+    name: '10th Special Sitting - Minutes of Proceedings',
+    url: minutes5,
+  },
+  {
+    name: 'Minutes of Proceedings of NGT Committee - 28th Meeting',
+    url: minutes6,
+  },
+  {
+    name: '9th Special Sitting - Minutes of Proceedings',
+    url: minutes7,
+  },
+  {
+    name: 'Minutes of Proceedings of NGT Committee - 27th Meeting',
+    url: minutes8,
+  },
+  {
+    name: 'Minutes of Proceedings of NGT Committee - 26th Meeting',
+    url: minutes9,
+  },
+  {
+    name: 'Minutes of Proceedings of NGT Committee - 25th Meeting',
+    url: minutes10,
+  },
+  {
+    name: 'Minutes of Proceedings of NGT Committee - 24th Meeting',
+    url: minutes11,
+  },
+  {
+    name: 'Minutes of Proceedings of NGT Committee - 23rd Meeting',
+    url: minutes12,
+  },
+  {
+    name: 'Minutes of Proceedings of NGT Committee - 22nd Meeting',
+    url: minutes13,
+  },
+  {
+    name: '7th Special Sitting - Minutes of Proceedings',
+    url: minutes14,
+  },
+  {
+    name: '6th Special Sitting - Minutes of Proceedings',
+    url: minutes15,
+  },
+  {
+    name: 'Minutes of Proceedings of NGT Committee - 21st Meeting',
+    url: minutes16,
+  },
+  {
+    name: '5th Special Sitting - Minutes of Proceedings',
+    url: minutes17,
+  },
+  {
+    name: 'Minutes of Proceedings of NGT Committee - 20th Meeting',
+    url: minutes18,
+  },
+  {
+    name: '3rd Special Sitting - Minutes of Proceedings',
+    url: minutes19,
+  },
+  {
+    name: 'Minutes of Proceedings of NGT Committee - 19th Meeting',
+    url: minutes20,
+  },
+  {
+    name: 'Minutes of Proceedings of NGT Committee - 18th Meeting',
+    url: minutes21,
+  },
+  {
+    name: '2nd Special Sitting - Minutes of Proceedings',
+    url: minutes22,
+  },
+  {
+    name: 'Minutes of Proceedings of NGT Committee - 17th Meeting',
+    url: minutes23,
+  },
+  {
+    name: 'Minutes of Proceedings NGT Committee - 16th Meeting - 2nd day',
+    url: minutes24,
+  },
+  {
+    name: 'Minutes of Proceedings NGT Committee - 16th Meeting - 1st day',
+    url: minutes25,
+  },
+  {
+    name: '1st Special Sitting - Minutes of Proceedings',
+    url: minutes26,
+  },
+  {
+    name: 'Minutes of Proceedings of NGT Committee - 15th Meeting',
+    url: minutes27,
+  },
+  {
+    name: 'Minutes of Proceedings NGT Committee - Site Visit during 14th Meeting',
+    url: minutes28,
+  },
+  {
+    name: 'Minutes of Proceedings of NGT Committee - 14th Meeting',
+    url: minutes29,
+  },
+  {
+    name: 'Minutes of Proceedings of NGT Committee - 13th Meeting',
+    url: minutes30,
+  },
+  {
+    name: 'Minutes of Proceedings of NGT Committee - 12th Meeting',
+    url: minutes31,
+  },
+  {
+    name: 'Minutes of Proceedings of NGT Committee - 11th Meeting',
+    url: minutes32,
+  },
+  {
+    name: 'Minutes of Proceedings of NGT Committee - 10th Meeting',
+    url: minutes33,
+  },
+  {
+    name: 'Minutes of Proceedings of NGT Committee - 9th Meeting',
+    url: minutes34,
+  },
+  {
+    name: 'Minutes of Proceedings of NGT Committee - 8th Meeting',
+    url: minutes35,
+  },
+  {
+    name: 'Minutes of Proceedings of NGT Committee - 7th Meeting',
+    url: minutes36,
+  },
+  {
+    name: 'Minutes of Proceedings of NGT Committee - 6th Meeting',
+    url: minutes37,
+  },
+  {
+    name: 'Minutes of Proceedings of NGT Committee - 5th Meeting',
+    url: minutes38,
+  },
+  {
+    name: 'Minutes of Proceedings of NGT Committee - 4th Meeting',
+    url: minutes39,
+  },
+  {
+    name: 'Minutes of Proceedings of NGT Committee - 3rd Meeting',
+    url: minutes40,
+  },
+  {
+    name: 'Minutes of Proceedings of NGT Committee - 2nd Meeting',
+    url: minutes41,
+  },
+  {
+    name: 'Minutes of Proceedings of NGT Committee - 1st Meeting',
+    url: minutes42,
+  },
+]
