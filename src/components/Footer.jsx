da2fff67ac1e73af3b5ce6990319f4c82ea70c9e
Dalam-Cajee <dalamcajee1999@gmail.com>
2024-03-20 18:02:31 +0530

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,15 @@
 import React from "react"
-import { NavLink } from "react-bootstrap"
+import { Col, NavLink, Row } from "react-bootstrap"
+import logo from "../assets/ForestDeptLogo.png"
 
 const Footer = () => {
   return (
-    <footer className="text-bg-dark">
+    <footer className="text-bg-dark p-2">
+      <Row>
+        <Col>
+          <img src={logo} alt="Logo" height={150} />
+        </Col>
+      </Row>
       <div className="container-fluid py-3 text-center">
         <div className="fs-small">
           Contents Owned, Maintained and Updated by the Forest and Environment
@@ -16,9 +22,7 @@ const Footer = () => {
         <div className="fs-small">
           Developed and hosted by National Informatics Center, Meghalaya
         </div>
-        <div className="fs-small">
-          <NavLink href="/terms">Terms of Use</NavLink>
-        </div>
+        <hr />
       </div>
     </footer>
   )
