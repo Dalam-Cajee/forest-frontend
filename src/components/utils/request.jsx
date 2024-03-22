f6ec3b53b7b53f1ff32b4361021f64a33d1ec3e6
Dalam-Cajee <dalamcajee1999@gmail.com>
2024-03-22 17:58:13 +0530

    changes in the request file

--- src/components/utils/request.jsx
+++ src/components/utils/request.jsx
@@ -2,7 +2,6 @@ import axios from "axios"
 
 const client = axios.create({ baseURL: "http://10.179.2.82:8082/wildlife-mis" })
 // const client = axios.create({ baseURL: 'http://localhost:8080' })
-// const client = axios.create({ baseURL: "http://10.179.2.82:8082/wildlife-mis" })
 
 export const request = ({ ...options }) => {
   if (localStorage.getItem("token"))
